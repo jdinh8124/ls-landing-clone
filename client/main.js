5f41d87cd4477c57547e457c0bb930c50760ac88
James <mrjames8124@gmail.com>
2020-03-19 13:11:08 -0700

    button center and dynamically shows a new section of departments:

--- client/main.js
+++ client/main.js
@@ -15,8 +15,10 @@ $('#button-expand').on('click', function(){
   if ($('#hidden-dept').hasClass('hidden')){
     $('#hidden-dept').removeClass('hidden');
     $('#button-expand').text('View Less')
+    $("#dept").addClass("expand");
   }else{
     $('#hidden-dept').addClass('hidden');
     $('#button-expand').text('View More')
+    $("#dept").removeClass("expand");
   }
 })
