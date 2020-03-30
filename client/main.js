6d642f46d7ae9e844cc37897c7f7dc1e8fedb554
James <mrjames8124@gmail.com>
2020-03-30 11:01:34 -0700

    corrected spelling on intro modal

--- client/main.js
+++ client/main.js
@@ -4,14 +4,14 @@ function initalizeApp() {
   $('.single-item').slick({
     arrows: true,
     accessibility: true,
-    speed: 800,
+    speed: 1500,
     autoplay: true,
     slidesToShow: 1
   });
   $('#header-message-slide').slick({
     arrows: false,
     accessibility: true,
-    speed: 800,
+    speed: 900,
     autoplay: true,
     slidesToShow: 1
   });
